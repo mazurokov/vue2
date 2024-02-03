505c539d84d64b79a16c92fc1f72b848d3a335fe
mazurokov <mazurok.oleksandr.v@gmail.com>
2024-02-03 07:14:16 +0200

    transition to vite

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
-<template>
-  <div id="app">
-    <HeaderTemplate/>
-    <router-view/>
-  </div>
-</template>
-
-<script>
-
-// @ is an alias to /src
-import HeaderTemplate from '@/components/Header/Header.vue'
-
-export default {
-  name: 'HomePage',
-  components: {
-    HeaderTemplate,
-  }
-}
+<script setup>
+import { RouterView } from 'vue-router'
+/*
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+*/
+import HeaderComponents from '@/components/Header/Header.vue'
 
 </script>
+
+<template>
+  <HeaderComponents/>
+  <RouterView />
+</template>
 
 <style lang="sass" src="@/assets/sass/style.sass"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchPage/SearchPage.vue'
 
-import SearchPage from '../views/SearchPage/SearhcPage.vue'
-
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/search',
-    name: 'SearchPage',
-    component: SearchPage
-  },
-]
-
-const router = new VueRouter({
-  routes
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/search/',
+      name: 'home',
+      component: SearchView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
