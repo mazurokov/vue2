494e4e504c8f62b0088b5ef235deecf7b2f7e688
mazurok <oleksandr.mazurok@auto.ria.com>
2025-06-18 14:20:07 +0300

    test-video

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <div style="max-width: 100%; overflow-x: auto">
-      <pre v-if="mediaError">
-        mediaError::: {{ mediaError }}
-      </pre>
+    <div style="">
+      <pre v-if="videoError">
+      videoError::: {{videoError}}
+    </pre>
       <br>
       <pre v-if="videoErrorMessage">
-        videoErrorMessage::: {{ videoErrorMessage }}
-      </pre>
+      videoErrorMessage::: {{videoErrorMessage}}
+    </pre>
     </div>
     <div v-if="data?.withoutPlayback" class="without-playback">
       Відео недоступне для відтворення
     </div>
     <video
+      v-else
       ref="videoPlayer"
       crossorigin
       playsinline
@@ -30,47 +31,43 @@ export default {
   data() {
     return {
       meta: null,
-      mediaError: null,
-      videoErrorMessage: ''
-    }
+      videoError: null,
+      videoErrorMessage: '',
+    };
   },
   mounted() {
     const video = this.$refs.videoPlayer;
-    if (this.data?.file && !this.data?.withoutPlayback) {
-      video.src = URL.createObjectURL(this.data.file);
-    }
+
     video.addEventListener('error', () => {
       const error = video.error;
       if (error) {
         this.videoError = {
-          code: error?.code,
-          message: error?.message,
+          code: error.code,
+          message: error.message,
         };
-
         switch (error.code) {
           case MediaError.MEDIA_ERR_ABORTED:
-            console.error('Відтворення відео перервано користувачем.');
+            this.videoErrorMessage = 'Відтворення відео перервано користувачем.';
             break;
           case MediaError.MEDIA_ERR_NETWORK:
-            console.error('Помилка мережі під час завантаження відео.');
+            this.videoErrorMessage = 'Помилка мережі під час завантаження відео.';
             break;
           case MediaError.MEDIA_ERR_DECODE:
-            console.error('Помилка декодування відео.');
-            this.mediaError = true;
+            this.videoErrorMessage = 'Помилка декодування відео.';
             break;
           case MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED:
-            console.error('Формат відео або тип не підтримується.');
-            this.mediaError = true;
+            this.videoErrorMessage = 'Формат відео або MIME-тип не підтримується.';
             break;
           default:
-            console.error('Невідома помилка відео.');
-            this.mediaError = true;
+            this.videoErrorMessage = 'Невідома помилка відео.';
             break;
         }
       }
     });
 
-
-  }
-}
+    if (this.data?.file && !this.data?.withoutPlayback) {
+      video.src = URL.createObjectURL(this.data?.file);
+    }
+  },
+};
 </script>
