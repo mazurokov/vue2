0b5395ae70b3f4342cc0b386367b055fff7e95a0
mazurok <oleksandr.mazurok@auto.ria.com>
2025-06-18 12:14:50 +0300

    test-video

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -26,6 +26,32 @@ export default {
   },
   mounted() {
     const video = this.$refs.videoPlayer;
+
+    video.addEventListener('error', (event) => {
+      const error = video.error;
+      if (error) {
+        console.error('Video error code:', error.code);
+
+        switch (error.code) {
+          case MediaError.MEDIA_ERR_ABORTED:
+            console.error('Відтворення відео перервано користувачем.');
+            break;
+          case MediaError.MEDIA_ERR_NETWORK:
+            console.error('Помилка мережі під час завантаження відео.');
+            break;
+          case MediaError.MEDIA_ERR_DECODE:
+            console.error('Помилка декодування відео.');
+            break;
+          case MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED:
+            console.error('Формат відео або MIME-тип не підтримується.');
+            break;
+          default:
+            console.error('Невідома помилка відео.');
+            break;
+        }
+      }
+    });
+
     if (this.data?.file && !this.data?.withoutPlayback) {
       video.src = URL.createObjectURL(this.data?.file);
     }
